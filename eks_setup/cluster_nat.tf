eafe4867cf61fef0d299023caffec27c6f9cd8df
Havryliuk-Pavlo-Maksymyliano <antihetmandb@gmail.com>
2025-11-15 19:50:46 +0200

    TAT-79: creation of basic eks cluster

---
+++ eks_setup/cluster_nat.tf
@@ -0,0 +1,18 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.environment_name}-${var.eks_cluster_name}-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_zone1.id
+
+  tags = {
+    Name = "${var.environment_name}-${var.eks_cluster_name}-nat"
+  }
+
+  depends_on = [data.aws_route_table.public_rote_table]
+}
---
+++ eks_setup/cluster_subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "private_zone1" {
+  vpc_id            = data.aws_vpc.vpc_id.id
+  cidr_block        = var.private_subnet_cidr_block_1
+  availability_zone = var.cluster_availability_zone_1
+
+  tags = {
+    "Name"                                                                  = "${var.environment_name}-${var.eks_cluster_name}-private-${var.cluster_availability_zone_1}"
+    "kubernetes.io/role/internal-elb"                                       = "1"
+    "kubernetes.io/cluster/${var.environment_name}-${var.eks_cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_zone2" {
+  vpc_id            = data.aws_vpc.vpc_id.id
+  cidr_block        = var.private_subnet_cidr_block_2
+  availability_zone = var.cluster_availability_zone_2
+
+  tags = {
+    "Name"                                                                  = "${var.environment_name}-${var.eks_cluster_name}-private-${var.cluster_availability_zone_2}"
+    "kubernetes.io/role/internal-elb"                                       = "1"
+    "kubernetes.io/cluster/${var.environment_name}-${var.eks_cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone1" {
+  vpc_id                  = data.aws_vpc.vpc_id.id
+  cidr_block              = var.public_subnet_cidr_block_1
+  availability_zone       = var.cluster_availability_zone_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                                  = "${var.environment_name}-${var.eks_cluster_name}-public-${var.cluster_availability_zone_1}"
+    "kubernetes.io/role/elb"                                                = "1"
+    "kubernetes.io/cluster/${var.environment_name}-${var.eks_cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone2" {
+  vpc_id                  = data.aws_vpc.vpc_id.id
+  cidr_block              = var.public_subnet_cidr_block_2
+  availability_zone       = var.cluster_availability_zone_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                                  = "${var.environment_name}-${var.eks_cluster_name}-public-${var.cluster_availability_zone_2}"
+    "kubernetes.io/role/elb"                                                = "1"
+    "kubernetes.io/cluster/${var.environment_name}-${var.eks_cluster_name}" = "owned"
+  }
+}
