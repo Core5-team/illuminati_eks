ec47bd6531bd5df943a946e9ff049028bf42fdff
olehbulka <oleg.bulka@gmail.com>
2025-12-10 13:46:46 +0200

    CORE5-32: add main.tf to create s3 bucket and IAM roles for Illuminati backend

---
+++ modules/irsa_s3_bucket/main.tf
@@ -0,0 +1,56 @@
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.aws_eks_cluster.cluster.name
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+data "aws_iam_policy_document" "irsa_trust" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_eks_cluster.cluster.identity[0].oidc.issuer]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_eks_cluster.cluster.identity[0].oidc.issuer,"https://","")}:sub"
+      values   = ["system:serviceaccount:${var.namespace}:${var.service_account_name}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "irsa_role" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.irsa_trust.json
+}
+
+resource "aws_iam_role_policy" "s3_policy" {
+  name   = "${var.role_name}-s3-policy"
+  role   = aws_iam_role.irsa_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:GetObject","s3:PutObject","s3:DeleteObject","s3:ListBucket"],
+      Resource = [
+        aws_s3_bucket.bucket.arn,
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }]
+  })
+}
