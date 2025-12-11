02f019e9baadb166ba4fdc7c40e1c5ff4a30c68a
olehbulka <oleg.bulka@gmail.com>
2025-12-11 12:26:27 +0200

    CORE5-32: delete unnecessary decryption of CA cretificate

--- modules/irsa_s3_bucket/main.tf
+++ modules/irsa_s3_bucket/main.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
   host                   = var.cluster_endpoint
   token                  = var.cluster_token
-  cluster_ca_certificate = base64decode(var.cluster_certificate_authority)
+  cluster_ca_certificate = var.cluster_certificate_authority
 }
 
 locals {
